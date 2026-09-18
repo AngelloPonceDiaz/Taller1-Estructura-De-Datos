7be8cae8c3f031c220708afbda4e3cfeebafd42d
AngelloPonceDiaz <angello.ponce@alumnos.ucn.cl>
2026-09-18 17:03:20 -0300

    Agregada la funcion mostrarPaciente para que deje de llover y no tener que hacer codigos en el 18

--- Departamentos.cpp
+++ Departamentos.cpp
@@ -1,5 +1,6 @@
 #include "Departamentos.h"
 #include <string>
+#include <iostream>
 
 Departamentos::Departamentos(string nombre){
     this->nombre = nombre;
@@ -11,4 +12,25 @@ void Departamentos::agregarPaciente(Paciente* paciente){
     Node* nodo = new Node(paciente);
     nodo->next = cabezaPaciente;
     cabezaPaciente = nodo;
+}
+
+void Departamentos::mostrarPacientes() const {
+    int cantPaciente = 0;
+    if (cabezaPaciente == nullptr) {
+        cout << "  No hay pacientes en " << nombre << ".\n";
+        return;
+    }
+
+    Node* actual = cabezaPaciente;
+    Node* aux = cabezaPaciente;
+
+    while(aux != nullptr){cantPaciente++;}
+
+    cout << " ===== Pacientes en " << nombre << ": "<< cantPaciente << " ===== ";
+    while(actual != nullptr){
+        Paciente* p = actual->paciente;
+
+        cout << p->getNombre() << " (" << p->getEdad() << ")\n";
+        actual = actual->next;
+    }
 }
